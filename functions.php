9d0c53a23b111892fbf0ad2caedb29a7c78dea47
teebark <terry@teebark.com>
2022-07-11 17:16:02 -0400

    baseline

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+/*
+add_action( 'after_setup_theme', function() {
+    add_theme_support( 'editor-color-palette', array(
+        array(
+            'name'  => esc_html__( 'Blue' ),
+            'slug'  => 'blue',
+            'color' => '#59BACC',
+        ),
+        array(
+            'name'  => esc_html__( 'Green' ),
+            'slug'  => 'green',
+            'color' => '#58AD69',l
+        ),
+        array(
+            'name'  => esc_html__( 'Orange' ),
+            'slug'  => 'orange',
+            'color' => '#FFBC49',
+        ),
+        array(
+            'name'  => esc_html__( 'Red' ),
+            'slug'  => 'red',
+            'color' => '#E2574C',
+        ),
+    ) );
+} );
+*/
+?>
